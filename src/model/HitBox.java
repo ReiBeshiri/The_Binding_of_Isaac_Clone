f8ccc43bf3fa968fd413063b08201202aadd8300
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-03-30 09:36:24 +0200

    Changed name of package and set formatter rules

---
+++ src/model/HitBox.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * 
+ *
+ */
+public interface HitBox {
+    /**
+     * Get X of center of HitBox.
+     * @return X coordinate.
+     */
+    double getX();
+    /**
+     * Get Y of center of HitBox.
+     * @return Y coordinate.
+     */
+    double getY();
+}
