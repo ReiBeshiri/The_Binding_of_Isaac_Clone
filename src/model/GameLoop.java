b8ff9410573927c6ad0bbdd717ea5d23e840085a
Rei Beshiri <rei.beshiri@studio.unibo.it>
2018-04-05 22:30:28 +0200

    added GameLoop interface

---
+++ src/model/GameLoop.java
@@ -0,0 +1,10 @@
+package model;
+/**
+ * 
+ * @author Rey
+ *     Note: when the GameLoopImpl class will be added this class should implement Runnable
+ *      and not extends Thread so it can extends some other utility class
+ */
+public interface GameLoop {
+
+}
