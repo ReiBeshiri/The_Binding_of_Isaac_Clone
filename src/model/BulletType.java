3f0f2ee2c59dcefc4b4db224b0a7ef38dfd6234c
Rei Beshiri <rei.beshiri@studio.unibo.it>
2018-04-05 21:37:59 +0200

    checkstyle comments

--- src/model/BulletType.java
+++ src/model/BulletType.java
@@ -20,32 +20,30 @@ public enum BulletType {
     private final double damage;
     private final double range;
     private final double steps;
-    
-    BulletType(final double damage, final double range, final double steps){
-        this.damage=damage;
-        this.range=range;
-        this.steps=steps;
+    BulletType(final double damage, final double range, final double steps) {
+        this.damage = damage;
+        this.range = range;
+        this.steps = steps;
     }
-    /*
-     * Get the value of the damage
+    /**
+     * Get the value of the damage.
      * @return The damage made by the bullet
      */
     public double getDamage() {
         return damage;
     }
-    /*     
-     * Get the value of the range
+    /**
+     * Get the value of the range.
      * @return The range of the bullet
      */
     public double getRange() {
         return range;
     }
-    /*
-     * Get the value of the steps
+    /**
+     * Get the value of the steps.
      * @return The steps of the bullet
      */
     public double getSteps() {
         return steps;
     }
-    
 }
