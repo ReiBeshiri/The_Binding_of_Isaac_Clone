f597a41b010ade1204965a1b21030d01f915110e
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-04-22 10:12:38 +0200

    Completed class for a generic scene

--- src/view/AbstractGenericScene.java
+++ src/view/AbstractGenericScene.java
@@ -1,13 +1,56 @@
 package view;
 
 import input.SceneType;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
 
 /**
- * 
- *
+ * Class that represent a generic scene.
+ * Some method need to be defined in a specific concrete subclass.
  */
 public abstract class AbstractGenericScene {
-    public AbstractGenericScene(SceneType s) {
-        
+    private final SceneType scene;
+    private final EventHandler<KeyEvent> sceneHandler;
+    /**
+     * Constructor for a generic scene.
+     * @param s Scene type.
+     */
+    public AbstractGenericScene(final SceneType s) {
+        scene = s;
+        sceneHandler = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(final KeyEvent event) {
+                if (event.getCode().toString().equals("ESC") && event.getEventType().equals(KeyEvent.KEY_PRESSED)) {
+                    exitStatus();
+                }
+                checkSceneHandler(event);
+            }
+        };
+    }
+    /**
+     * Method called when 'esc' command in pressed.
+     * It depends on a type of scene.
+     */
+    public abstract void exitStatus();
+    /**
+     * Method used to check key events when handler captures these.
+     * It depends on a type of scene.
+     * @param e Event captured by the handler.
+     */
+    public abstract void checkSceneHandler(Event e);
+    /**
+     * Get the scene type.
+     * @return The type of scene, represented by a enum type.
+     */
+    public SceneType getSceneType() {
+        return this.scene;
+    }
+    /**
+     * Getter for a event handler.
+     * @return The event handler for the scene.
+     */
+    public EventHandler<KeyEvent> getEventHandler() {
+        return this.sceneHandler;
     }
 }
