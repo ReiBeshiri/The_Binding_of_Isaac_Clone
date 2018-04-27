bb3c0fc3f31e270139103fb4c4d127bf0e0b1d16
Rei Beshiri <rei.beshiri@studio.unibo.it>
2018-04-27 21:30:54 +0200

    completed SpawnsUtility

--- src/utility/SpawnUtility.java
+++ src/utility/SpawnUtility.java
@@ -5,8 +5,9 @@ package utility;
 public final class SpawnUtility {
     private SpawnUtility() {
     }
-    private static final double XSTANDARD = ProportionUtility.getWidth() / 8;
+    private static final double XSTANDARD = ProportionUtility.getWidth() / 5;
     private static final double YSTANDARD = ProportionUtility.getHeigth() / 4;
+    //this first set of spawns are the spawn in the top of the map.
     /**
      * @return x axis spawn A.
      */
@@ -18,5 +19,90 @@ public final class SpawnUtility {
      */
     public static double getSpawnAY() {
         return YSTANDARD;
+    }
+    /**
+     * @return x axis spawn B.
+     */
+    public static double getSpawnBX() {
+        return XSTANDARD * 2;
+    }
+    /**
+     * @return y axis spawn B.
+     */
+    public static double getSpawnBY() {
+        return YSTANDARD;
+    }
+    /**
+     * @return x axis spawn C.
+     */
+    public static double getSpawnCX() {
+        return XSTANDARD * 3;
+    }
+    /**
+     * @return y axis spawn C.
+     */
+    public static double getSpawnCY() {
+        return YSTANDARD;
+    }
+    /**
+     * @return x axis spawn D.
+     */
+    public static double getSpawnDX() {
+        return XSTANDARD * 4;
+    }
+    /**
+     * @return y axis spawn D.
+     */
+    public static double getSpawnDY() {
+        return YSTANDARD;
+    }
+  //this second set of spawns are the spawn in the bottom of the map.
+    /**
+     * @return x axis spawn E.
+     */
+    public static double getSpawnEX() {
+        return XSTANDARD;
+    }
+    /**
+     * @return y axis spawn E.
+     */
+    public static double getSpawnEY() {
+        return YSTANDARD * 3;
+    }
+    /**
+     * @return x axis spawn F.
+     */
+    public static double getSpawnFX() {
+        return XSTANDARD * 2;
+    }
+    /**
+     * @return y axis spawn F.
+     */
+    public static double getSpawnFY() {
+        return YSTANDARD * 3;
+    }
+    /**
+     * @return x axis spawn G.
+     */
+    public static double getSpawnGX() {
+        return XSTANDARD * 3;
+    }
+    /**
+     * @return y axis spawn G.
+     */
+    public static double getSpawnGY() {
+        return YSTANDARD * 3;
+    }
+    /**
+     * @return x axis spawn H.
+     */
+    public static double getSpawnHX() {
+        return XSTANDARD * 4;
+    }
+    /**
+     * @return y axis spawn H.
+     */
+    public static double getSpawnHY() {
+        return YSTANDARD * 3;
     }
 }
