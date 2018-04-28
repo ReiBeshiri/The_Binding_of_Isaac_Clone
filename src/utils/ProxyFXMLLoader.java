d397d77a73a08d9f2072315a664ecc9d30b2b44f
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-04-28 12:13:51 +0200

    Added proxy image loader

--- src/utils/ProxyFXMLLoader.java
+++ src/utils/ProxyFXMLLoader.java
@@ -46,11 +46,6 @@ public final class ProxyFXMLLoader implements LoaderFXML {
         }
         return proxy;
     }
-
-    /**
-     * Class that load FXML files from disk.
-     *
-     */
     private class RealFXMLLoader implements LoaderFXML {
         private static final String PATH = "/res/";
 
