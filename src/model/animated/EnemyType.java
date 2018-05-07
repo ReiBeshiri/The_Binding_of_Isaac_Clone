704d983df622758871bc21f1cb973941416ef009
Rei Beshiri <rei.beshiri@studio.unibo.it>
2018-05-07 17:35:47 +0200

    created enum type enemy

---
+++ src/model/animated/EnemyType.java
@@ -0,0 +1,19 @@
+package model.animated;
+/**
+ * Enum of enemy types.
+ */
+public enum EnemyType {
+    /**
+     * Create Static Simple Direction Shot Enemy.
+     */
+    SIPMLE,
+    /**
+     * Create Static Aimed Bullet Enemy.
+     */
+    SIMPLEAIMED,
+    /**
+     * Create Simple Direction Moved Enemy.
+     */
+    SIMPLEMOVE;
+
+}
