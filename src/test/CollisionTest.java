93712c7cf0f70b9d66a8b514a1145fc6806e5eb5
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-07-25 11:53:25 +0200

    Fixed related errors in collision test

--- src/test/CollisionTest.java
+++ src/test/CollisionTest.java
@@ -8,8 +8,6 @@ import model.animated.Animated;
 import model.animated.BulletImpl;
 import model.hitbox.CircleHitBox;
 import model.hitbox.RectangularHitBox;
-import model.room.MainRoom;
-import model.room.Room;
 import model.strategy.BulletMovement;
 import proxyutility.ImageType;
 import utility.CollisionUtil;
@@ -62,7 +60,7 @@ class CollisionTest {
     @Test
     public void collisionBetweenEntityAndRoomBorders() {
         //TOP BORDER COLLISION.
-        final Room room = new MainRoom(new RectangularHitBox(0, 0, 600, 800), null, null, null);
+        final RectangularHitBox room = new RectangularHitBox(0, 0, 600, 800);
         final CircleHitBox player = new CircleHitBox(3, 2, 2);
         assertTrue(CollisionUtil.checkBoundaryCollision(player, room));
 
