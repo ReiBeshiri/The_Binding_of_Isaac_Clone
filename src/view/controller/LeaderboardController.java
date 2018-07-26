c41054e9ef0f305fa0b5b583dca1521a6f34f982
Denis.Cristurean <denis.cristurean@studio.unibo.it>
2018-07-26 13:58:04 +0200

    added references in the code for the leadeboard

--- src/view/controller/LeaderboardController.java
+++ src/view/controller/LeaderboardController.java
@@ -1,6 +1,9 @@
 package view.controller;
 
+import controller.util.Score;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
 import view.ViewManagerImpl;
@@ -11,11 +14,24 @@ import view.ViewManagerImpl;
 public class LeaderboardController extends AbstractControllerFXML {
 
     @FXML
+    private TableView<Score> table;
+    @FXML
+    private TableColumn<Score, String> nicknameColumn;
+    @FXML
+    private TableColumn<Score, Integer> pointsColumn;
+    @FXML
+    private TableColumn<Score, String> timeColumn;
+    @FXML
     private BorderPane contentPane;
 
     @FXML
     private void backButtonClick() {
         super.closingFade(() -> ViewManagerImpl.get().pop());
+    }
+
+    @FXML
+    public void initialize() {
+
     }
 
     @Override
