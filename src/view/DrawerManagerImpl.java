5544df7ad4fd299a4a730cbe37730b56ada2ff1f
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-07-30 20:06:01 +0200

    Removed unused imports

--- src/view/DrawerManagerImpl.java
+++ src/view/DrawerManagerImpl.java
@@ -1,12 +1,10 @@
 package view;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.IntStream;
 import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
