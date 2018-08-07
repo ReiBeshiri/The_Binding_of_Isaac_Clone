54d38c1fa72c488cef4a46c2d0970adbee861f7d
Denis.Cristurean <denis.cristurean@studio.unibo.it>
2018-08-07 22:03:35 +0200

    Added to the Pause view extra labels

--- src/view/controller/PauseViewController.java
+++ src/view/controller/PauseViewController.java
@@ -4,11 +4,13 @@ import controller.event.ButtonEventImpl;
 import controller.utility.ButtonType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
 import view.MainMenuScene;
 import view.ViewImpl;
 import view.ViewManagerImpl;
+import view.utility.ViewUtils;
 
 /**
  * 
@@ -20,6 +22,18 @@ public class PauseViewController extends AbstractControllerFXML {
     @FXML private BorderPane contentPane;
     @FXML private Button exitButton;
     @FXML private Button resumeButton;
+    @FXML private Label scoreLabel;
+    @FXML private Label damageLabel;
+    @FXML private Label velLabel;
+    @FXML private Label bulletRangeLabel;
+
+    @FXML
+    private void initialize() {
+        scoreLabel.setText("");
+        damageLabel.setText("");
+        velLabel.setText("");
+        bulletRangeLabel.setText("");
+    }
 
     @Override
     public final Region getRoot() {
@@ -28,18 +42,15 @@ public class PauseViewController extends AbstractControllerFXML {
 
     @FXML
     private void resumeButtonClick() {
-        // super.closingFade(() -> ViewManagerImpl.get().pop());
         ViewManagerImpl.get().pop();
         ViewImpl.get().notifyEvent(new ButtonEventImpl(ButtonType.RESUME_GAME, ""));
     }
 
     @FXML
     private void exitButtonClick() {
-        // super.closingFade(() -> ViewManagerImpl.get().push(new MainMenuScene()));
         ViewImpl.get().notifyEvent(new ButtonEventImpl(ButtonType.RETURN_MAIN_MENU, ""));
         while (!(ViewManagerImpl.get().getCurrentScene() instanceof MainMenuScene)) {
             ViewManagerImpl.get().pop();
         }
-        // ViewManagerImpl.get().push(SceneFactory.createMenuScene());
     }
 }
