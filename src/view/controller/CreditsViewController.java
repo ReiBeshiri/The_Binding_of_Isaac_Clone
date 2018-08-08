0ab7a2bda3d62b1a2db4f5719f3e1b475704afe8
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-08-08 21:05:18 +0200

    Fixed minor bugs

--- src/view/controller/CreditsViewController.java
+++ src/view/controller/CreditsViewController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
 import view.ViewManagerImpl;
+
 /**
  * Controller class for the CreditsView file.
  */
