79f3c751b18d87140eddca2a5ca51d17b645fcf0
Andrea Vaienti <andrea.vaienti2@studio.unibo.it>
2018-08-08 21:54:53 +0200

    Fixed findbugs error

--- src/controller/utility/ScoreImpl.java
+++ src/controller/utility/ScoreImpl.java
@@ -64,4 +64,30 @@ public class ScoreImpl implements Score {
         }
         return this.point - player.getPoint();
     }
+
+    @Override
+    public final int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + point;
+        return result;
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ScoreImpl other = (ScoreImpl) obj;
+        if (point != other.point || other.getTime().getTimeInSeconds() != this.time.getTimeInSeconds()) {
+            return false;
+        }
+        return true;
+    }
 }
