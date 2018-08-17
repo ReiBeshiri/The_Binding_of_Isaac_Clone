b35162f0a23714790078dd95825f20907d18d779
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2018-08-17 11:08:23 +0200

    Added time test

---
+++ src/test/TestTime.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import controller.time.Time;
+import controller.time.TimeAgent;
+
+class TestTime {
+
+    @Test
+    public void testTimeThread() {
+        final Time time = new Time(0, 0);
+        final TimeAgent timeAgent = new TimeAgent(time);
+        assertTrue(timeAgent.isRunning());
+        timeAgent.interrupt();
+        final int finish = time.getTimeInSeconds();
+        assertFalse(timeAgent.isRunning());
+        assertEquals(finish, time.getTimeInSeconds());
+    }
+
+    @Test
+    public void testTimeToString() {
+        final Time time = new Time(0, 0);
+        final String timeToString = time.toString();
+        final List<String> times = Arrays.asList(timeToString.split(":"));
+        times.forEach(x -> assertTrue(x.length() == 2));
+    }
+}
